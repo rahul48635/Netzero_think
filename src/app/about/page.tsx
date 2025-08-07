4f8eee502b63e16ff73f5543c602de097249de68
rahul48635 <rahulsingh48635@gmail.com>
2025-08-07 22:23:22 +0530

    refined about page animations

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -68,9 +68,9 @@ export default function AboutPage() {
           </h1>
         <div className="flex flex-col md:flex-row justify-between items-baseline mx-5 bg-blue-900 rounded-2xl">
           <motion.div
-              initial={{ x: isMobile?-100:-300 }}
+              initial={{ x: isMobile?-10:-300 }}
               whileInView={{ x: 1 }}
-              transition={{ duration: 1 }}
+              transition={{ duration: 0.7 }}
               className="text-transparent font-bold text-2xl sm:text-3xl md:text-4xl z-20 flex flex-col gap-10 lg:basis-1/2 text-center lg:text-left m-5 rounded-2xl justify-center"
             >
                     <h1 className="text-white text-2xl">
@@ -84,9 +84,9 @@ export default function AboutPage() {
                     </h1>
           </motion.div>
           <motion.div
-          initial={{ x: isMobile?150:300 }}
+          initial={{ x: isMobile?10:300 }}
               whileInView={{ x: 1 }}
-              transition={{ duration: 1 }}
+              transition={{ duration: 0.7 }}
               className="text-transparent font-bold text-2xl sm:text-3xl md:text-4xl z-20 flex flex-col gap-10 lg:basis-1/2 text-center lg:text-left m-5 "
           >
             <div className="flex flex-wrap gap-10 items-center justify-center">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <body
+      suppressHydrationWarning={true}
        className={`${geistSans.variable} ${geistMono.variable} antialiased  `}
       >
         <DarkModeProvider>
