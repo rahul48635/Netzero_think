3703df25a855a86bbce3c3fd7e5755e54f83bb0c
rahul48635 <rahulsingh48635@gmail.com>
2025-09-02 02:14:14 +0530

    changed team

--- src/app/components/Contactbox.tsx
+++ src/app/components/Contactbox.tsx
@@ -53,34 +53,40 @@ const ChatBox = () => {
       initial={{ opacity: 0, scale: 0, transformOrigin: "bottom right" }}
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 0 }}
-      transition={{ duration: 0.5,ease:"easeInOut" }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
       className="w-80 h-[30rem] bg-white dark:bg-black shadow-xl rounded-2xl p-4 flex flex-col absolute bottom-20 right-0 z-100"
     >
       <div className="text-lg font-semibold mb-2 border-b pb-2">
         💬 Chat with us on
       </div>
 
-
       <div className="flex flex-col max-h-full items-center justify-around gap-5">
         <motion.button
           className="bg-green-500 text-white px-4 py-2 rounded-xl text-sm cursor-pointer w-full"
           whileTap={{ scale: 1.2 }}
-          onClick={()=>window.open(`https://wa.me/${whatsappNumber}`)}
+          onClick={() => window.open(`https://wa.me/${whatsappNumber}`)}
         >
           WhatsApp
         </motion.button>
         <motion.button
           className="bg-blue-500 text-white px-4 py-2 rounded-xl text-sm cursor-pointer w-full"
           whileTap={{ scale: 1.2 }}
-          onClick={()=>window.open("https://www.facebook.com/p/NET-ZERO-THINK-100078446962916/?paipv=0&eav=AfbufXp8uGtISCG_iy2czsdtQ26X2s_mPiSk8kf5sPQoy09ZH2yUGQvIi02y_AzhYFU&_rdr")}
+          onClick={() =>
+            window.open(
+              "https://www.facebook.com/p/NET-ZERO-THINK-100078446962916/?paipv=0&eav=AfbufXp8uGtISCG_iy2czsdtQ26X2s_mPiSk8kf5sPQoy09ZH2yUGQvIi02y_AzhYFU&_rdr"
+            )
+          }
         >
           FaceBook
         </motion.button>
         <motion.button
           className="bg-green-800 text-white px-4 py-2 rounded-xl text-sm cursor-pointer w-full"
           whileTap={{ scale: 1.2 }}
-          onClick={()=>window.open(`mailto:support@netzerothink.com?subject=New Service Request from [Name]&body=Name: [Name]%0APhone: [WhatsApp Number]%0AOrganistion: [Your Org]%0ADesignation: [Your Designation]%0ALinkedIn: [Link]%0ADescription: [Why do you want to Join?]`)}
-
+          onClick={() =>
+            window.open(
+              `mailto:support@netzerothink.com?subject=New Service Request from [Name]&body=Name: [Name]%0APhone: [WhatsApp Number]%0AOrganistion: [Your Org]%0ADesignation: [Your Designation]%0ALinkedIn: [Link]%0ADescription: [Why do you want to Join?]`
+            )
+          }
         >
           Join Our Community
         </motion.button>
@@ -94,7 +100,9 @@ const ChatBox = () => {
         <motion.button
           className="bg-blue-700 text-white px-4 py-2 rounded-xl text-sm cursor-pointer w-full"
           whileTap={{ scale: 1.2 }}
-          onClick={()=>window.open("https://www.linkedin.com/company/net-zero-think/")}
+          onClick={() =>
+            window.open("https://www.linkedin.com/company/net-zero-think/")
+          }
         >
           LinkedIn
         </motion.button>
