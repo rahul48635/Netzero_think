e1415f93be552988c1ca56d803b5a219405003b3
rahul48635 <rahulsingh48635@gmail.com>
2026-04-02 06:10:27 +0530

    some files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,14 @@
-import type { Config } from 'tailwindcss';
-import tailwindScrollbar from 'tailwind-scrollbar';
+import type { Config } from "tailwindcss";
+import tailwindScrollbar from "tailwind-scrollbar";
 
 const config: Config = {
   darkMode: "class",
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/app/components/**/*.{js,ts,jsx,tsx}",
-    "./src/app/context/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      scrollbars: {
+        custom: "",
+      },
       borderRadius: {
         "4xl": "2rem",
       },
@@ -19,13 +17,11 @@ const config: Config = {
         background: "var(--background)",
       },
       boxShadow: {
-        cus: "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px"
+        cus: "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
       },
     },
   },
-  plugins: [
-    tailwindScrollbar,
-  ],
+  plugins: [tailwindScrollbar({ nocompatible: true })],
 };
 
 export default config;
